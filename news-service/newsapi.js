58c702e03d32a7ad78131c8e97dc21faea1985b7
Andrey Smolko <andr.smolko@gmail.com>
2021-02-23 20:19:52 +0300

    minor refactoring of fetchArticles

--- news-service/newsapi.js
+++ news-service/newsapi.js
@@ -5,27 +5,19 @@ const NewsAPI = require('newsapi')
 const newsapi = new NewsAPI(process.env.NEWS_API_KEY).v2
 
 module.exports.headlines = async (params) => newsapi.topHeadlines(params)
-
 module.exports.everything = async (params) => newsapi.everything(params)
 
 module.exports.fetchArticles = async (type, params) => {
+  let data
   switch (type) {
     case 'headlines':
-      return exports
-        .headlines(params)
-        .then((data) => {
-          console.log('found: ', data.totalResults)
-          return data.articles
-        })
-        .catch((e) => console.error(e))
+      data = await exports.headlines(params)
+      console.log('found: ', data.totalResults)
+      return data.articles
     case 'search':
-      return exports
-        .everything(params)
-        .then((data) => {
-          console.log('found: ', data.totalResults)
-          return data.articles
-        })
-        .catch((e) => console.error(e))
+      await exports.everything(params)
+      console.log('found: ', data.totalResults)
+      return data.articles
     default:
       console.log('none')
       break
